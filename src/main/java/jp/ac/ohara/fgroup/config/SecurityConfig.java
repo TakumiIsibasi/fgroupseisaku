08d7b0438bbe545a924de1988e703c60f928d615
MasatoshiAdachi <fko2347043@stu.o-hara.ac.jp>
2024-05-07 10:47:18 +0900

    n

--- src/main/java/jp/ac/ohara/fgroup/config/SecurityConfig.java
+++ src/main/java/jp/ac/ohara/fgroup/config/SecurityConfig.java
@@ -1,12 +1,12 @@
 package jp.ac.ohara.fgroup.config;
-
+ 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-
+ 
+ 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
