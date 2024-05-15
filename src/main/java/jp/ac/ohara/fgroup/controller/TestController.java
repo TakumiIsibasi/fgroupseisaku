625bf7f9eadabb99b13ce7b6afc71df79a3a6491
TakumiIsibasi <fko2347037@stu.o-hara.ac.jp>
2024-05-15 09:50:17 +0900

    aaa

---
+++ src/main/java/jp/ac/ohara/fgroup/controller/TestController.java
@@ -0,0 +1,12 @@
+package jp.ac.ohara.fgroup.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+ 
+@Controller
+public class TestController {
+	@GetMapping("/test")
+    public String index() {
+        return "test";
+    }
+}
---
+++ src/main/java/jp/ac/ohara/fgroup/service/TestService.java
@@ -0,0 +1,34 @@
+package jp.ac.ohara.fgroup.service;
+ 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
+import jp.ac.ohara.fgroup.model.TestModel;
+import jp.ac.ohara.fgroup.repository.TestRepository;
+ 
+@Service
+@Transactional
+public class TestService {
+ 
+    @Autowired
+    private TestRepository repository;
+ 
+    public List<TestModel> getGradesList() {
+        return repository.findAll();
+    }
+ 
+    public TestModel get(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+ 
+    public void save(TestModel grades) {
+        repository.save(grades);
+    }
+ 
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
